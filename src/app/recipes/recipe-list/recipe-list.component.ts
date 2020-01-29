ccb7725772c16bb62e1ce6a7afeeaffcd3310b7a
bfly2001 <brendan.sflynn@gmail.com>
2020-01-29 09:28:59 -0800

    Imported Recipe model for use in RecipeListComponent.
    Imported Recipe model for use in RecipeListComponent.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  
+
   constructor() { }
 
   ngOnInit() {
